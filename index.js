894264118ab63b570483e8cd2b7fd306b5e3f579
ilovepreet <82634332+ilovepreet@users.noreply.github.com>
2025-04-10 22:03:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./exotic-38593-firebase-adminsdk-fbsvc-7bcd091105.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
+
 
 const db = admin.firestore();
 const app = express();
